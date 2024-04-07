257dee67ed8d7cc02d6f2710aaf27e015a3364b1
truemat <matvei.truhan@gmail.com>
2024-04-07 23:47:46 +0300

    task 3

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void askName() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("May I have your name?");
+        var name = in.nextLine();
+        System.out.println("Hello, "+name);
+    }
+
+}
