9af42824f68f73a8419e490c9a0f19a179f9297e
truemat <matvei.truhan@gmail.com>
2024-04-23 15:14:49 +0300

    update

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,13 +9,13 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class App {
-    final static int GREET = 1;
-    final static int EVEN = 2;
-    final static int CALC = 3;
-    final static int GCD = 4;
-    final static int PROGRESSION = 5;
-    final static int PRIME = 6;
-    final static int EXIT = 0;
+    static final int GREET = 1;
+    static final int EVEN = 2;
+    static final int CALC = 3;
+    static final int GCD = 4;
+    static final int PROGRESSION = 5;
+    static final int PRIME = 6;
+    static final int EXIT = 0;
 
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.\n"
@@ -31,24 +31,24 @@ public class App {
         System.out.println("Your choice: " + playerChoice);
 
         switch (playerChoice) {
-            case App.EXIT:
+            case EXIT:
                 break;
-            case App.GREET:
+            case GREET:
                 Cli.askName();
                 break;
-            case App.EVEN:
+            case EVEN:
                 Even.startGame();
                 break;
-            case App.CALC:
+            case CALC:
                 Calc.startGame();
                 break;
-            case App.GCD:
+            case GCD:
                 Gcd.startGame();
                 break;
-            case App.PROGRESSION:
+            case PROGRESSION:
                 Progression.startGame();
                 break;
-            case App.PRIME:
+            case PRIME:
                 Prime.startGame();
                 break;
             default:
